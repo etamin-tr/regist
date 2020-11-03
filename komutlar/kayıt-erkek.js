90e938f37cedc1b0e8e96328ec17562827fe48fe
etamin-tr <72015139+etamin-tr@users.noreply.github.com>
2020-11-03 22:37:22 +0300

    Add files via upload

---
+++ komutlar/kayıt-erkek.js
@@ -0,0 +1,55 @@
+const Discord = require('discord.js')
+const db = require('quick.db')
+
+exports.run = async (client, message, args) => {
+
+ if(!['763769937273028608', '763720890860568647'].some(role => message.member.roles.cache.get(role)) && !message.member.hasPermission('ADMINISTRATOR')) return message.reply(`Bu Komut İçin Yetkiniz Bulunmamaktadır.`) 
+  
+let tag = "々"
+const kayıtlı = message.guild.roles.cache.find(r => r.id === '763719977865183262')
+const kayıtsız = message.guild.roles.cache.find(r => r.id === '763769165629751298')
+
+if(!kayıtlı) return message.reply('Kayıtlı Rolü Ayarlanmamış.') 
+if(!kayıtsız) return message.reply('Kayıtsız Rolü Ayarlanmamış.') 
+  
+let member = message.mentions.users.first() || client.users.cache.get(args.join(' '))
+if(!member) return message.channel.send('Kimi Kayıt Etmem Gerekiyor ?')
+let stg = message.guild.member(member)
+let isim = args[1]
+let yas = args[2]
+if(!isim) return message.reply('')
+if(!yas) return message.reply('')
+
+stg.setNickname(`${tag} ${isim} ' ${yas}`)  
+stg.roles.add(kayıtlı)
+stg.roles.remove(kayıtsız)
+
+db.add(`kayıtSayi.${message.author.id}`, 1)
+db.add(`erkekUye.${message.author.id}`, 1)
+let erkek = db.get(`erkekUye.${message.author.id}`);
+let kayıtlar = db.fetch(`kayıtSayi.${message.author.id}`); 
+  
+const embed = new Discord.MessageEmbed()
+.setTitle(`Kayıt İşlemi Tamamlandı`)
+    .addField(`Kayıt Eden:`, `<@${message.author.id}> Tarafından Kayıt Edildi`) 
+    .addField(`Kayıt Edilen:`, `<@${stg.user.id}> Kayıt Oldu`)
+    .addField(`Verilen Rol:`, `<@&${kayıtlı.id}> Rolleri Verildi`) 
+    .addField(`Alınan Rol:`, `<@&${kayıtsız.id}> Rolleri Alındı`)
+    .addField(`Yeni İsmin:`, `\`${tag} ${isim} ' ${yas}\` Olarak Güncellendi`) 
+    .addField(`Yetkili Toplam:`, `\`${kayıtlar}\` Kayıtlara Sahip.`)
+.setFooter(`Hypnos <3`)
+.setColor('GREEN')
+client.channels.cache.get('763717206386409472').send(embed)
+  
+}
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ['erkek','e','man','boy'],
+    permLevel: 0
+};
+
+exports.help = {
+    name: 'erkek',
+};
